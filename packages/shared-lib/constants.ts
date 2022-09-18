93f69e929d26f5be67621724a2dfae1fd652f81a
Nico Mendoza <blankartwp@gmail.com>
2022-09-18 15:08:27 +0800

    fix: tube-hub to rooms2watch const names

--- packages/shared-lib/constants.ts
+++ packages/shared-lib/constants.ts
@@ -1,6 +1,6 @@
 export const ACCESS_TOKEN_KEY = "__rooms2watch-access-token";
 
-export const CHAT_NAME_KEY = "__tube-hub-user-name";
+export const CHAT_NAME_KEY = "__rooms2watch-user-name";
 
 export const CHAT_LOCAL_STORAGE_SESSION_KEY =
-  "__tube-hub-local-storage-session-id";
+  "__rooms2watch-local-storage-session-id";
