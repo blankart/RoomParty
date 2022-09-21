ba508d361fb3ccb996aa480bd077f1159b3b11b2
Nico Mendoza <blankartwp@gmail.com>
2022-09-21 21:03:29 +0800

    fix: attempt to fix transpiled modules

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,11 @@
-// const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require("next-transpile-modules")(['@rooms2watch/shared-lib']);
 const { withSuperjson } = require("next-superjson");
 
 // module.exports = withTM({
 //   reactStrictMode: true,
 // });
 
-module.exports = withSuperjson()({
+module.exports = withTM(withSuperjson()({
   // reactStrictMode: true,
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-});
+})) ;
