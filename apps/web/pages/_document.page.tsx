53cccb29fb8f1212217e70e5ef04918f9249650a
Nico Mendoza <blankartwp@gmail.com>
2022-09-22 22:48:52 +0800

    fix: attempt to fix zooming on textarea on mobile

--- apps/web/pages/_document.page.tsx
+++ apps/web/pages/_document.page.tsx
@@ -9,7 +9,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
