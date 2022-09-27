0e118ce88d94d6b7ff2aa8474572c65052f21457
Nico Mendoza <blankartwp@gmail.com>
2022-09-27 21:11:26 +0800

    refactor: refactor types

--- packages/trpc/dto.ts
+++ packages/trpc/dto.ts
@@ -1,3 +1,5 @@
 export * as RoomsDTO from "./src/modules/rooms/rooms.dto";
 export * as ChatsDRO from "./src/modules/chats/chats.dto";
 export * as FavoritedRoomsDTO from "./src/modules/favorited-rooms/favorited-rooms.dto";
+export * as PlayerDTO from "./src/modules/player/player.dto";
+export * as VideoChatDTO from "./src/modules/video-chat/video-chat.dto";
--- packages/trpc/schema.ts
+++ packages/trpc/schema.ts
@@ -1,3 +1,5 @@
 export * as RoomsSchema from "./src/modules/rooms/rooms.schema";
 export * as ChatsSchema from "./src/modules/chats/chats.schema";
 export * as FavoritedRoomsSchema from "./src/modules/favorited-rooms/favorited-rooms.schema";
+export * as PlayerSchema from "./src/modules/player/player.schema";
+export * as VideoChatSchema from "./src/modules/video-chat/video-chat.schema";
