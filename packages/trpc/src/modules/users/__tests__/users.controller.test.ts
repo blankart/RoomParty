5b5b24fc1ccb7164a7f240b76b8bc8269a1d9df3
Nico Mendoza <blankartwp@gmail.com>
2022-10-08 20:48:50 +0800

    feat: added signIn tests

--- packages/trpc/src/modules/users/__tests__/users.controller.test.ts
+++ packages/trpc/src/modules/users/__tests__/users.controller.test.ts
@@ -1,15 +1,17 @@
-import { DeepMockProxy, mockDeep } from "jest-mock-extended";
+import { DeepMockProxy, mockClear, mockDeep } from "jest-mock-extended";
 import ModelsService from "../../models/models.service";
 import UsersController from "../users.controller";
 import { expect } from "@jest/globals";
 import EmailService from "../../email/email.service";
 import UsersService from "../users.service";
+import { createAuthProviderJwt } from "@RoomParty/auth-providers";
 
 describe("users.controller", () => {
   let modelsServiceMock: DeepMockProxy<ModelsService>;
   let emailServiceMock: DeepMockProxy<EmailService>;
   let usersServiceMock: DeepMockProxy<UsersService>;
   let usersController: UsersController;
+  let jwtMock: DeepMockProxy<ReturnType<typeof createAuthProviderJwt>>
 
   beforeEach(() => {
     modelsServiceMock = mockDeep<ModelsService>();
@@ -20,6 +22,7 @@ describe("users.controller", () => {
       emailServiceMock,
       usersServiceMock
     );
+    jwtMock = mockDeep<ReturnType<typeof createAuthProviderJwt>>()
   });
 
   test("me", async () => {
@@ -33,4 +36,33 @@ describe("users.controller", () => {
       },
     });
   });
+
+  test('signIn', async () => {
+    (modelsServiceMock as any).client.account.findFirst.mockImplementation(() => null)
+    let maybeWillThrow = async () => await usersController.signIn({ email: 'email@email.com', password: 'PASSWORD' }, jwtMock)
+
+    await expect(maybeWillThrow).rejects.toThrow();
+
+    (modelsServiceMock as any).client.account.findFirst.mockImplementation(() => ({}))
+
+    await expect(maybeWillThrow).rejects.toThrow();
+
+    (modelsServiceMock as any).client.account.findFirst.mockImplementation(() => ({ password: 'PASSWORD' }))
+
+    await expect(maybeWillThrow).rejects.toThrow();
+
+    (usersServiceMock as any).comparePasswordHash.mockImplementation(() => true)
+
+    await maybeWillThrow()
+
+    expect(jwtMock.signer).toBeCalled()
+    expect(jwtMock.signer).toBeCalledTimes(1)
+  })
+
+  afterEach(() => {
+    mockClear(modelsServiceMock)
+    mockClear(emailServiceMock)
+    mockClear(usersServiceMock)
+    mockClear(jwtMock)
+  })
 });
